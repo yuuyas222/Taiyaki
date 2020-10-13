542f1879ea2c4658c5d537a2006a4ef5d3626794
佐藤　祐弥 <y.satou@satouyuuwatarunoMacBook-Air.local>
2020-10-14 00:38:39 +0900

    finish Taiyaki

--- Contents.swift
+++ Contents.swift
@@ -1,9 +1,12 @@
 struct Taiyaki {
     var nakami = "あんこ"
+    var number = "4"
     func sayNakami() {
-        print("中身は" + nakami + "です。")
+        print("中身が" + nakami + "なのは" + number + "個ある。")
     }
 }
 var taiyaki = Taiyaki()
 taiyaki.nakami = "クリーム"
+taiyaki.number = "2"
 taiyaki.sayNakami()
+
