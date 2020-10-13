29418a7b5530d8e533f6b53433f4950b1bb0ddfa
佐藤　祐弥 <y.satou@satouyuuwatarunoMacBook-Air.local>
2020-10-13 18:35:49 +0900

    first commit

---
+++ Contents.swift
@@ -0,0 +1,9 @@
+struct Taiyaki {
+    var nakami = "あんこ"
+    func sayNakami() {
+        print("中身は" + nakami + "です。")
+    }
+}
+var taiyaki = Taiyaki()
+taiyaki.nakami = "クリーム"
+taiyaki.sayNakami()
